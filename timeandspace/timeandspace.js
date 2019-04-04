58ff19830b0c50a63f11eee4376c44ab82e23df3
Sarah Schöttler <sarah@schoettler.email>
2019-04-04 15:31:10 +0100

    add codes count timeline

--- timeandspace/timeandspace.js
+++ timeandspace/timeandspace.js
@@ -100,7 +100,7 @@ d3.csv("../data/paxTimelineData_02092018.csv", function(d) {
 	resetFilters()
 
 	// draw timeline
-	initTimeline(data, years, 1)
+	initTimeline(data, years)
 
 
 
@@ -133,7 +133,7 @@ d3.csv("../data/paxTimelineData_02092018.csv", function(d) {
 						cons: getSelectedCons(cons),
 						codes: getSelectedCodes()}
 
-			initTimeline(filterData(data, filters), filters.year, 1)
+			initTimeline(filterData(data, filters), filters.year)
 			updateGlyphs(filterData(locdata, filters))
 		})
 	
--- timeandspace/timeline.js
+++ timeandspace/timeline.js
@@ -1,16 +1,18 @@
 // maybe split into init and update function
 // add filters
 
-function initTimeline(data, years, chart) {
+function initTimeline(data, years) {
 
 	// empty div
-	d3.select('#timeline-top').selectAll('*').remove()
+	const chart = d3.select('#timeline-top .charts')
+	chart.selectAll('*').remove()
+	console.log(chart)
 
-	var margin = {top: 5, right: 5, bottom: 25, left: 5}, //read clockwise from top
-		width = parseInt(d3.select("#timeline-top").style("width"), 10) - margin.left - margin.right,
-		height = parseInt(d3.select("#timeline-top").style("height"), 10) - margin.top - margin.bottom;
+	var margin = {top: 25, right: 20, bottom: 40, left: 40}, //read clockwise from top
+		width = parseInt(chart.style("width"), 10) - margin.left - margin.right,
+		height = parseInt(chart.style("height"), 10) - margin.top - margin.bottom;
 
-	var g = d3.select('#timeline-top').append('svg')
+	var g = chart.append('svg')
 		.attr('height', height + margin.top + margin.bottom)
 		.attr('width', width + margin.left + margin.right)
 		.attr('id', 'svg-timeline-top')
@@ -37,64 +39,187 @@ function initTimeline(data, years, chart) {
 		.attr("class","xaxis")
 		.call(xAxis);
 
-	gChart = g.append('g')
+	// SWITCH BETWEEN TIMELINES ------------------------------------------------
+	const gBars = g.append('g').attr('visibility', 'hidden')
+	const gLines = g.append('g').attr('id', 'gLines').attr('visibility', 'visible')
 
-	if (chart === 1) {
+	d3.select('#timeline-top').selectAll('input').on('change', function() {
+		if (this.value == 'bars') {
+			gBars.attr('visibility', 'visible')
+			gLines.attr('visibility', 'hidden')
+		} 
+		else {
+			gBars.attr('visibility', 'hidden')
+			gLines.attr('visibility', 'visible')
+		}
+	})
 
-		var agtHeight = 1,
-			agtSpacing = 1;
 
-		// calculate agtWidth
-		var agtWidth = (width / (1 + years[1] - years[0])) - agtSpacing;
-		
-		// Group agreements by Year (create an array of objects whose key is the
-		// year and value is an array of objects (one per agreement))
-		var years = d3.nest()
-			.key(function(d){ return d.Year; }).sortKeys(d3.ascending)
-			.sortValues(function(a,b){ return d3.descending(a.Dat, b.Dat) })
-			.entries(data);
 
-		for (var i = 0; i < years.length; i++) {
-					
-			var rects = gChart.selectAll("rect .y" + i)
-				.data(years[i].values)
-				.enter()
-				.append("rect")
-				.classed('y' + i, true)
-				.attr('id', d => 'rect' + d.AgtId)
-				.attr("x", function(d){ return xScale(parseYear(d.Year)) - (agtWidth/2); })
-				.attr("y",function(d,i){ return height - ((agtHeight + agtSpacing) * i) })
-				.attr("width", agtWidth)
-				.attr("height", agtHeight)
-				.style('fill', '#000');
+	// STACKED RECTANGLES BAR CHART --------------------------------------------
 
-			rects.on("click", function(d) {
-				selectedAgt.clickOn(d)
-				event.stopPropagation();
-			})
-			.on("mouseover", onmouseover)
-			.on("mouseout", onmouseout);
+	var agtHeight = 1,
+		agtSpacing = 1;
 
-		} // end for loop (years)
+	// calculate agtWidth
+	var agtWidth = (width / (1 + years[1] - years[0])) - agtSpacing;
+	
+	// Group agreements by Year (create an array of objects whose key is the
+	// year and value is an array of objects (one per agreement))
+	var years = d3.nest()
+		.key(function(d){ return d.Year; }).sortKeys(d3.ascending)
+		.sortValues(function(a,b){ return d3.descending(a.Dat, b.Dat) })
+		.entries(data);
 
-		// add count at the top of each bar
-		var text = g.append('g')
-			.selectAll("text .count")
-			.data(years)   //yrCounts format: [Year, yrCount]
+	for (var i = 0; i < years.length; i++) {
+				
+		var rects = gBars.selectAll("rect .y" + i)
+			.data(years[i].values)
 			.enter()
-			.append("text")
-			.attr("class","count")
-			.attr("x", function(d){ return xScale(parseYear(d.key)) })
-			.attr("y", function(d){ return height - (d.values.length * (agtHeight + agtSpacing)) })
-			.text(function(d){ return d.values.length; })
-	} // end lines chart
+			.append("rect")
+			.classed('y' + i, true)
+			.attr('id', d => 'rect' + d.AgtId)
+			.attr("x", function(d){ return xScale(parseYear(d.Year)) - (agtWidth/2); })
+			.attr("y",function(d,i){ return height - ((agtHeight + agtSpacing) * i) })
+			.attr("width", agtWidth)
+			.attr("height", agtHeight)
+			.style('fill', '#000');
 
-	else if (chart === 2) {
+		rects.on("click", function(d) {
+			selectedAgt.clickOn(d)
+			event.stopPropagation();
+		})
+		.on("mouseover", onmouseover)
+		.on("mouseout", onmouseout);
 
+	} // end for loop (years)
+
+	// add count at the top of each bar
+	var text = gBars.append('g')
+		.selectAll("text .count")
+		.data(years)   //yrCounts format: [Year, yrCount]
+		.enter()
+		.append("text")
+		.attr("class","count")
+		.attr("x", function(d){ 
+			return xScale(parseYear(d.key)) 
+		})
+		.attr("y", function(d){ 
+			return height - (d.values.length * (agtHeight + agtSpacing)) 
+		})
+		.text(function(d){ return d.values.length; })
+
+
+
+
+	// LINE CHART --------------------------------------------------------------
+
+	// console.log(years)
+	var tooltip = gLines.append("g")
+	    .attr("transform", "translate(-100,-100)")
+	    .attr("id", "tooltip")
+		.style("pointer-events", "none");
+
+	tooltip.append("circle")
+		.attr("class", "tooltipCircle")
+	    .attr("r", 3);
+
+	tooltip.append("text")
+		.attr("class", "tooltipText")
+	    .attr("y", -12);
+
+	var dataLines = d3.nest()
+		.key(function(d) { return +d.Year; })
+		.rollup(function(v) {
+			var arr = [];
+			codes.forEach(function(code) {
+				if (code == 'GeWom') {console.log(v)}
+				arr.push([code, d3.sum(v, function(d) { return d[code] > 0; })]); 
+			})
+			return arr
+			}) 
+		.entries(data);
+
+	var flatDataLines = []
+	dataLines.forEach(function(yr) {
+		yr.value.forEach(function(arr) {
+			flatDataLines.push({
+				year: yr.key,
+				code: arr[0],
+				count: arr[1]
+			})
+		})
+	})
+
+	dataLines = d3.nest()
+		.key(function(d) {return d.code})
+		.entries(flatDataLines)
+
+	var maxCount = d3.max(flatDataLines, d => d.count)
+
+	var yLines = d3.scaleLinear()
+		.domain([0, maxCount])
+		.range([height, 0])
+
+	var yAxis = d3.axisLeft(yLines)
+		.tickFormat(d3.format("d"))
+		.tickPadding([5]);
+
+	// draw axis into svg
+	gLines.append("g")
+		.attr("class","yaxis")
+		.call(yAxis);
+
+	var line = d3.line()
+		.x(d => xScale(parseYear(d.year))) // set the x values for the line generator
+		.y(d => yLines(d.count)) // set the y values for the line generator 
+
+	var lines1 = gLines.selectAll('.codeline')
+		.data(dataLines)
+		.enter()
+		.append("path")
+		.datum(d => d.values.sort(function(a,b) {return d3.ascending(a.year,b.year)}))
+		.attr("d", line)
+		.attr('id', function(d) {return 'line' + d[0].code})
+		.style('stroke', d => codeColour(d[0].code))
+		.style('stroke-linejoin', "round")
+		.style('stroke-width', '4px')
+		.style('opacity', 0.8)
+		.style('fill', 'none')
+		.attr('pointer-events', 'none')
+
+	const delaunay = d3.Delaunay.from(flatDataLines,
+		d => xScale(parseYear(d.year)), // x accessor
+		d => yLines(d.count)) 			// y accessor
+
+	const voronoi = delaunay.voronoi([0, 0, width, height]);
+
+	gLines.append('g')
+		.selectAll('path')
+		.data(flatDataLines)
+		.enter()
+		.append('path')
+		.attr('d', function(d,i) { return voronoi.renderCell(i) })
+		.classed('voronoicell', true)
+		// .style('stroke', 'black') // uncomment to see voronoi cells
+		.style('fill', 'transparent')
+		.on('mouseover', mouseover)
+		.on('mouseout', mouseout)
+
+	//Voronoi mouseover and mouseout functions	
+	function mouseover(d) {
+		lines1.style('opacity', 0.1)
+		d3.select('#line' + d.code).style('opacity', 0.8).moveToFront()
+		
+		tooltip.moveToFront();
+	    tooltip.attr("transform", "translate(" + xScale(parseYear(d.year)) + "," + yLines(d.count) + ")");
+		tooltip.select(".tooltipCircle").style("fill", codeColour(d.code)).attr("r", 5);
+	    tooltip.select(".tooltipText").text(d.code + ': ' + d.count);
 	}
 
-	else {
-		// chart == 'ridge'
-		console.log('error: unknown chart type', chart)
+	function mouseout(d) {
+		lines1.style('opacity', 0.8)	    
+		tooltip.attr("transform", "translate(-100,-100)");
 	}
+
 }
