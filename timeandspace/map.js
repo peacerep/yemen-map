fca72951ecef2b14d11f2fa10f2627c189ab3288
sarah37 <sarah@schoettler.email>
2019-04-25 16:36:09 +0200

    jsprettier

--- timeandspace/map.js
+++ timeandspace/map.js
@@ -1,50 +1,56 @@
-"use strict"
+"use strict";
 
 // get width and height
-var w = parseInt(d3.select("#map").style("width"))
-var h = parseInt(d3.select("#map").style("height"))
+var w = parseInt(d3.select("#map").style("width"));
+var h = parseInt(d3.select("#map").style("height"));
 
 // create svg
-var svg = d3.select("#map")
+var svg = d3
+	.select("#map")
 	.append("svg")
 	.attr("width", w)
 	.attr("height", h);
 
 // initial scale and translation (makes mercator projection fit screen)
-var scaleInit = h/(2*Math.PI) * 1.7
-var transInit = [w/2, h*0.6]
+var scaleInit = (h / (2 * Math.PI)) * 1.7;
+var transInit = [w / 2, h * 0.6];
 
 // define projection
-var projection = d3.geoMercator()
+var projection = d3
+	.geoMercator()
 	.scale(scaleInit)
-	.translate(transInit)
+	.translate(transInit);
 
 // define path generator
-var path = d3.geoPath()
-	.projection(projection);
+var path = d3.geoPath().projection(projection);
 
-var mapG = svg.append('g').attr('id', 'mapG') // g for the map
-var dotG = svg.append('g').attr('id', 'dotG') // g for dots or anything else we plot on top
+var mapG = svg.append("g").attr("id", "mapG"); // g for the map
+var dotG = svg.append("g").attr("id", "dotG"); // g for dots or anything else we plot on top
 
-var arc = d3.arc()
+var arc = d3
+	.arc()
 	.innerRadius(0)
 	.outerRadius(function(d) {
-		return d3.scaleSqrt().domain(codesRange[d.code]).range([0,15])(d.score)
+		return d3
+			.scaleSqrt()
+			.domain(codesRange[d.code])
+			.range([0, 15])(d.score);
 	})
-	.cornerRadius(5)
+	.cornerRadius(5);
 
 // vars for glyphs
 const rCircle = 4;
-const fillCircle = '#c4ccd0';
+const fillCircle = "#c4ccd0";
 
 // set up zoom
-var zoom = d3.zoom()
-	.scaleExtent([0.7,50])
-	.on("zoom", zooming)
+var zoom = d3
+	.zoom()
+	.scaleExtent([0.7, 50])
+	.on("zoom", zooming);
 
-svg.call(zoom)
+svg.call(zoom);
 
-svg.on('click', selectedAgt.clear)
+svg.on("click", selectedAgt.clear);
 
 function zooming() {
 	// keep stroke-width constant at different zoom levels
@@ -52,115 +58,145 @@ function zooming() {
 	// zoom map
 	mapG.attr("transform", d3.event.transform);
 	// semantic zoom flowers
-	dotG.selectAll('g').attr("transform", function(d) {
-		return 'translate(' + d3.event.transform.apply(projection(d.loc)) + ')';
+	dotG.selectAll("g").attr("transform", function(d) {
+		return "translate(" + d3.event.transform.apply(projection(d.loc)) + ")";
 	});
 	// make sure selected glyph, if any, stays 2x size
 	if (selectedAgt.get()) {
-		d3.select('#glyph' + selectedAgt.get().AgtId)
-			.attr('transform', function(d) {
-				return 'translate(' + d3.event.transform.apply(projection(d.loc)) + ') scale(2)';
-			})
+		d3.select("#glyph" + selectedAgt.get().AgtId).attr("transform", function(
+			d
+		) {
+			return (
+				"translate(" +
+				d3.event.transform.apply(projection(d.loc)) +
+				") scale(2)"
+			);
+		});
 	}
 }
 
 // buttons for zoom
-var zoomG = svg.append('g')
-	.attr('transform', 'translate(' + (w-35) + ',' + (h-60) + ')')
+var zoomG = svg
+	.append("g")
+	.attr("transform", "translate(" + (w - 35) + "," + (h - 60) + ")");
 
-zoomG.append('rect')
-	.attr('x', 0)
-	.attr('y', 0)
-	.attr('width', 25)
-	.attr('height', 25)
-	.style('fill', '#fff')
-	.style('stroke', '#000')
-	.style('stroke-width', '#1px')
-	.on('mouseover', function() {
-		d3.select(this).style('fill', '#ccc')
+zoomG
+	.append("rect")
+	.attr("x", 0)
+	.attr("y", 0)
+	.attr("width", 25)
+	.attr("height", 25)
+	.style("fill", "#fff")
+	.style("stroke", "#000")
+	.style("stroke-width", "#1px")
+	.on("mouseover", function() {
+		d3.select(this).style("fill", "#ccc");
 	})
-	.on('mouseout', function() {
-		d3.select(this).style('fill', '#fff')
+	.on("mouseout", function() {
+		d3.select(this).style("fill", "#fff");
 	})
-	.on('click', function() {
+	.on("click", function() {
 		// zoom in
-		zoom.scaleBy(svg.transition().duration(200), 1.3)
-	})
+		zoom.scaleBy(svg.transition().duration(200), 1.3);
+	});
 
-zoomG.append('rect')
-	.attr('x', 0)
-	.attr('y', 25)
-	.attr('width', 25)
-	.attr('height', 25)
-	.style('fill', '#fff')
-	.style('stroke', '#000')
-	.style('stroke-width', '#1px')
-	.on('mouseover', function() {
-		d3.select(this).style('fill', '#ccc')
+zoomG
+	.append("rect")
+	.attr("x", 0)
+	.attr("y", 25)
+	.attr("width", 25)
+	.attr("height", 25)
+	.style("fill", "#fff")
+	.style("stroke", "#000")
+	.style("stroke-width", "#1px")
+	.on("mouseover", function() {
+		d3.select(this).style("fill", "#ccc");
 	})
-	.on('mouseout', function() {
-		d3.select(this).style('fill', '#fff')
+	.on("mouseout", function() {
+		d3.select(this).style("fill", "#fff");
 	})
-	.on('click', function() {
-		zoom.scaleBy(svg.transition().duration(200), 1/1.3)
+	.on("click", function() {
+		zoom.scaleBy(svg.transition().duration(200), 1 / 1.3);
 		// zoom out
-	})
+	});
 
 // plus sign
-zoomG.append('line')
-	.attr('x1', 7.5).attr('x2', 17.5)
-	.attr('y1', 12.5).attr('y2', 12.5)
-	.style('stroke', '#000').attr('stroke-width', '2px')
-	.attr('pointer-events', 'none')
+zoomG
+	.append("line")
+	.attr("x1", 7.5)
+	.attr("x2", 17.5)
+	.attr("y1", 12.5)
+	.attr("y2", 12.5)
+	.style("stroke", "#000")
+	.attr("stroke-width", "2px")
+	.attr("pointer-events", "none");
 
-zoomG.append('line')
-	.attr('x1', 12.5).attr('x2', 12.5)
-	.attr('y1', 7.5).attr('y2', 17.5)
-	.style('stroke', '#000').attr('stroke-width', '2px')
-	.attr('pointer-events', 'none')
+zoomG
+	.append("line")
+	.attr("x1", 12.5)
+	.attr("x2", 12.5)
+	.attr("y1", 7.5)
+	.attr("y2", 17.5)
+	.style("stroke", "#000")
+	.attr("stroke-width", "2px")
+	.attr("pointer-events", "none");
 
 // minus sign
-zoomG.append('line')
-	.attr('x1', 7.5).attr('x2', 17.5)
-	.attr('y1', 37.5).attr('y2', 37.5)
-	.style('stroke', '#000').attr('stroke-width', '2px')
-	.attr('pointer-events', 'none')
-
+zoomG
+	.append("line")
+	.attr("x1", 7.5)
+	.attr("x2", 17.5)
+	.attr("y1", 37.5)
+	.attr("y2", 37.5)
+	.style("stroke", "#000")
+	.attr("stroke-width", "2px")
+	.attr("pointer-events", "none");
 
 function combineDataPoly(data, world) {
-
 	// filter for intra agreements only
 	var data_intra = data.filter(function(d) {
-		return d.Agtp == 'Intra'
-	})
+		return d.Agtp == "Intra";
+	});
 
 	// nest by the first country on the list
-	data_intra = d3.group(data_intra, d => d.Con[0])
+	data_intra = d3.group(data_intra, d => d.Con[0]);
 
-	var newData = []
+	var newData = [];
 
 	for (const [con, agts] of data_intra.entries()) {
-
-		var cen = world.features.find(function(d) {return d.properties.name == con})
+		var cen = world.features.find(function(d) {
+			return d.properties.name == con;
+		});
 
 		// Palestine and Kurdistan are not internationally recognised and therefore not on the map
 		// place them at their approximate location/within the recognised country
-		if (con == 'Palestine') {cen = world.features.find(function(d) {return d.properties.name == 'Israel'})}
-		if (con == 'Kurds-Kurdistan') {cen = world.features.find(function(d) {return d.properties.name == 'Iraq'})}
-		
-		if (cen != undefined) {
+		if (con == "Palestine") {
+			cen = world.features.find(function(d) {
+				return d.properties.name == "Israel";
+			});
+		}
+		if (con == "Kurds-Kurdistan") {
+			cen = world.features.find(function(d) {
+				return d.properties.name == "Iraq";
+			});
+		}
 
-			var pts = randomGeoPoints(cen.geometry, agts.length)
+		if (cen != undefined) {
+			var pts = randomGeoPoints(cen.geometry, agts.length);
 
 			// create one long list of all agreements
-			newData.push(...agts.map(function(d,i) { d.loc = pts[i]; return d }))
-
+			newData.push(
+				...agts.map(function(d, i) {
+					d.loc = pts[i];
+					return d;
+				})
+			);
+		} else {
+			console.log(con + " cannot be found on the map");
 		}
-		else {console.log(con + ' cannot be found on the map')}
 	}
 
-	return newData
-
+	return newData;
 }
 
 function updateGlyphs(locdata) {
@@ -171,133 +207,158 @@ function updateGlyphs(locdata) {
 
 	// filter data for the visible dots only, also filter out undef ones
 	var locdata = locdata.filter(function(d) {
-		return filterBBox(bbox, d.loc)
-	})
+		return filterBBox(bbox, d.loc);
+	});
 
-	dotG.selectAll('*').remove()
+	dotG.selectAll("*").remove();
 
 	// add new ones
-	var circle = dotG.selectAll('g')
+	var circle = dotG
+		.selectAll("g")
 		.data(locdata)
 		.enter()
 		.append("g")
 		.classed("glyph", true)
-		.attr('id', d => 'glyph' + d.AgtId)
+		.attr("id", d => "glyph" + d.AgtId)
 		// .merge(circle)
-		.attr('transform', function(d) {
-			return 'translate(' + d3.zoomTransform(svg.node()).apply(projection(d.loc)) + ')';
-		})
-	
-	circle.append('circle')
-		.attr('cx', 0)
-		.attr('cy', 0)
-		.attr('r', rCircle)
-		.style('fill', fillCircle)
-		.style('fill-opacity', 1)
+		.attr("transform", function(d) {
+			return (
+				"translate(" +
+				d3.zoomTransform(svg.node()).apply(projection(d.loc)) +
+				")"
+			);
+		});
 
-	circle.selectAll('.arc')
+	circle
+		.append("circle")
+		.attr("cx", 0)
+		.attr("cy", 0)
+		.attr("r", rCircle)
+		.style("fill", fillCircle)
+		.style("fill-opacity", 1);
+
+	circle
+		.selectAll(".arc")
 		.data(function(d) {
 			var incr = tau / codes.length;
-			var obj = []
+			var obj = [];
 
 			for (var i = 0; i < codes.length; i++) {
 				if (d[codes[i]]) {
 					obj.push({
 						startAngle: i * incr,
-						endAngle: (i+1) * incr,
+						endAngle: (i + 1) * incr,
 						code: codes[i],
 						score: d[codes[i]]
-					})
+					});
 				}
 			}
-			return obj
+			return obj;
 		})
 		.enter()
-		.append('path')
-		.attr('d', arc)
-		.style('fill', d => codeColour(d.code))
+		.append("path")
+		.attr("d", arc)
+		.style("fill", d => codeColour(d.code));
 
-	circle.on("click", function(d) {
-		selectedAgt.clickOn(d)
-		event.stopPropagation();
-	})
-	.on("mouseover", onmouseover)
-	.on("mouseout", onmouseout)
+	circle
+		.on("click", function(d) {
+			selectedAgt.clickOn(d);
+			event.stopPropagation();
+		})
+		.on("mouseover", onmouseover)
+		.on("mouseout", onmouseout);
 
-	return circle
+	return circle;
 }
 
 function getBoundingBox() {
 	// computes bounding box of currently visible part of the map in terms of lat/lon
 
-	var mapBounds = mapG.node().getBBox()
-	var mapTrans = d3.zoomTransform(svg.node())
+	var mapBounds = mapG.node().getBBox();
+	var mapTrans = d3.zoomTransform(svg.node());
 
-	var l = mapTrans.x + mapBounds.x * mapTrans.k
-	var r = l + mapBounds.width * mapTrans.k
-	var t = mapTrans.y + mapBounds.y * mapTrans.k
-	var b = t + mapBounds.height * mapTrans.k
+	var l = mapTrans.x + mapBounds.x * mapTrans.k;
+	var r = l + mapBounds.width * mapTrans.k;
+	var t = mapTrans.y + mapBounds.y * mapTrans.k;
+	var b = t + mapBounds.height * mapTrans.k;
 
 	// compare to bounds of svg (visible part)
-	l = (l > 0 ? -180 : projection.invert([0, NaN])[0])
-	r = (r < w ? 180 : projection.invert([w, NaN])[0])
-	t = (t > 0 ? 85 : projection.invert([NaN, 0])[1])
-	b = (b < h ? -85 : projection.invert([NaN, h])[1])
+	l = l > 0 ? -180 : projection.invert([0, NaN])[0];
+	r = r < w ? 180 : projection.invert([w, NaN])[0];
+	t = t > 0 ? 85 : projection.invert([NaN, 0])[1];
+	b = b < h ? -85 : projection.invert([NaN, h])[1];
 
-	return {l: l, r: r, t: t, b: b}
+	return { l: l, r: r, t: t, b: b };
 }
 
 function filterBBox(bbox, loc) {
 	// check if point at lon/lat is within the visible part of the map
-	return (loc != undefined) && (bbox.l < loc[0] && loc[0] < bbox.r && bbox.b < loc[1] && loc[1] < bbox.t)
+	return (
+		loc != undefined &&
+		(bbox.l < loc[0] && loc[0] < bbox.r && bbox.b < loc[1] && loc[1] < bbox.t)
+	);
 }
 
 function randomGeoPoints(geometry, nPoints) {
 	// creates a random point in a country as defined by its geometry
-	var bbox = d3.geoBounds(geometry)
+	var bbox = d3.geoBounds(geometry);
 	var w = bbox[0][0],
 		s = bbox[0][1],
 		e = bbox[1][0],
 		n = bbox[1][1];
 
-		// array for random points
-		var pts = []
+	// array for random points
+	var pts = [];
 
-		// if the longitude at the west end of the country is larger than at the
-		// east end, the country must span across the 180 line
-		if (w > e) { 
-			var prop = [[w, 180], [-180, e]].map(d => d[1] - d[0])
-			var cut = prop[0] / (prop[0] + prop[1])
+	// if the longitude at the west end of the country is larger than at the
+	// east end, the country must span across the 180 line
+	if (w > e) {
+		var prop = [[w, 180], [-180, e]].map(d => d[1] - d[0]);
+		var cut = prop[0] / (prop[0] + prop[1]);
 
-			// a while loop would be more elegant but also more dangerous
-			for (var i = 0; i<1000; i++) {
-
-				var rand = Math.random()
-				var randLon = (rand < cut ? 
-							   w + rand * d3.sum(prop) :
-							   -180 + (rand * d3.sum(prop)) - prop[0])
-				var randomPoint = [randLon,
-								   s + Math.random() * (n - s)];
-				var contained = d3.geoContains(geometry, randomPoint)
-				if (contained) { pts.push(randomPoint) }
-				if (pts.length >= nPoints) { break; }
+		// a while loop would be more elegant but also more dangerous
+		for (var i = 0; i < 1000; i++) {
+			var rand = Math.random();
+			var randLon =
+				rand < cut
+					? w + rand * d3.sum(prop)
+					: -180 + rand * d3.sum(prop) - prop[0];
+			var randomPoint = [randLon, s + Math.random() * (n - s)];
+			var contained = d3.geoContains(geometry, randomPoint);
+			if (contained) {
+				pts.push(randomPoint);
+			}
+			if (pts.length >= nPoints) {
+				break;
 			}
 		}
-		// if this isn't the case it's just regular maths
-		else { 
-			for (var i = 0; i<1000; i++) {
-				var randomPoint = [w + Math.random() * (e - w),
-								   s + Math.random() * (n - s)];
-				var contained = d3.geoContains(geometry, randomPoint)
-				if (contained) { pts.push(randomPoint) }
-				if (pts.length >= nPoints) { break; }
+	}
+	// if this isn't the case it's just regular maths
+	else {
+		for (var i = 0; i < 1000; i++) {
+			var randomPoint = [
+				w + Math.random() * (e - w),
+				s + Math.random() * (n - s)
+			];
+			var contained = d3.geoContains(geometry, randomPoint);
+			if (contained) {
+				pts.push(randomPoint);
+			}
+			if (pts.length >= nPoints) {
+				break;
 			}
 		}
+	}
 
-		// log warning if there are not enough points
-		if (pts.length < nPoints) {
-			console.log('WARNING! In ' + i + ' iterations, \
-			not enough points could be generated for ' + geometry)
-		}
-		return pts
+	// log warning if there are not enough points
+	if (pts.length < nPoints) {
+		console.log(
+			"WARNING! In " +
+				i +
+				" iterations, \
+			not enough points could be generated for " +
+				geometry
+		);
+	}
+	return pts;
 }
--- timeandspace/timeline.js
+++ timeandspace/timeline.js
@@ -1,60 +1,64 @@
-"use strict"
+"use strict";
 // maybe split into init and update function
 // add filters
 
 function initTimeline(data, years) {
-
 	// empty div
-	const chart = d3.select('#timeline-top .charts')
-	chart.selectAll('*').remove()
+	const chart = d3.select("#timeline-top .charts");
+	chart.selectAll("*").remove();
 
-	var margin = {top: 25, right: 20, bottom: 40, left: 40}, //read clockwise from top
+	var margin = { top: 25, right: 20, bottom: 40, left: 40 }, //read clockwise from top
 		width = parseInt(chart.style("width"), 10) - margin.left - margin.right,
 		height = parseInt(chart.style("height"), 10) - margin.top - margin.bottom;
 
-	var g = chart.append('svg')
-		.attr('height', height + margin.top + margin.bottom)
-		.attr('width', width + margin.left + margin.right)
-		.attr('id', 'svg-timeline-top')
-		.append('g')
-		.attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
-		.attr('id', 'g-timeline-top')
+	var g = chart
+		.append("svg")
+		.attr("height", height + margin.top + margin.bottom)
+		.attr("width", width + margin.left + margin.right)
+		.attr("id", "svg-timeline-top")
+		.append("g")
+		.attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+		.attr("id", "g-timeline-top");
 
-	var minDate = parseDate('30/06/' + (years[0] - 1))
-	var maxDate = parseDate('30/06/' + years[1])
+	var minDate = parseDate("30/06/" + (years[0] - 1));
+	var maxDate = parseDate("30/06/" + years[1]);
 
 	// create time scale and x axis
-	var xScale = d3.scaleTime()
-		.domain([minDate,maxDate])  // data space
-		.range([0, width]);  // display space
+	var xScale = d3
+		.scaleTime()
+		.domain([minDate, maxDate]) // data space
+		.range([0, width]); // display space
 
-	var xAxis = d3.axisBottom(xScale)
+	var xAxis = d3
+		.axisBottom(xScale)
 		.tickFormat(d3.timeFormat("%Y"))
 		.ticks(d3.timeYear.every(1))
 		.tickPadding([5]);
 
 	// draw axis into svg
 	g.append("g")
-		.attr('transform', 'translate(0, ' + (height + 4) + ')')
-		.attr("class","xaxis")
+		.attr("transform", "translate(0, " + (height + 4) + ")")
+		.attr("class", "xaxis")
 		.call(xAxis);
 
 	// SWITCH BETWEEN TIMELINES ------------------------------------------------
-	const gBars = g.append('g').attr('visibility', 'hidden')
-	const gLines = g.append('g').attr('id', 'gLines').attr('visibility', 'visible')
+	const gBars = g.append("g").attr("visibility", "hidden");
+	const gLines = g
+		.append("g")
+		.attr("id", "gLines")
+		.attr("visibility", "visible");
 
-	d3.select('#timeline-top').selectAll('input').on('change', function() {
-		if (this.value == 'bars') {
-			gBars.attr('visibility', 'visible')
-			gLines.attr('visibility', 'hidden')
-		} 
-		else {
-			gBars.attr('visibility', 'hidden')
-			gLines.attr('visibility', 'visible')
-		}
-	})
-
-
+	d3.select("#timeline-top")
+		.selectAll("input")
+		.on("change", function() {
+			if (this.value == "bars") {
+				gBars.attr("visibility", "visible");
+				gLines.attr("visibility", "hidden");
+			} else {
+				gBars.attr("visibility", "hidden");
+				gLines.attr("visibility", "visible");
+			}
+		});
 
 	// STACKED RECTANGLES BAR CHART --------------------------------------------
 
@@ -62,180 +66,228 @@ function initTimeline(data, years) {
 		agtSpacing = 1;
 
 	// calculate agtWidth
-	var agtWidth = (width / (1 + years[1] - years[0])) - agtSpacing;
-	
+	var agtWidth = width / (1 + years[1] - years[0]) - agtSpacing;
+
 	// Group agreements by Year (create an array of objects whose key is the
 	// year and value is an array of objects (one per agreement))
-	var dataByYear = d3.nest()
-		.key(function(d){ return d.Year; }).sortKeys(d3.ascending)
-		.sortValues(function(a,b){ return d3.descending(a.Dat, b.Dat) })
+	var dataByYear = d3
+		.nest()
+		.key(function(d) {
+			return d.Year;
+		})
+		.sortKeys(d3.ascending)
+		.sortValues(function(a, b) {
+			return d3.descending(a.Dat, b.Dat);
+		})
 		.entries(data);
 
 	for (var i = 0; i < dataByYear.length; i++) {
-				
-		var rects = gBars.selectAll("rect .y" + i)
+		var rects = gBars
+			.selectAll("rect .y" + i)
 			.data(dataByYear[i].values)
 			.enter()
 			.append("rect")
-			.classed('y' + i, true)
-			.attr('id', d => 'rect' + d.AgtId)
-			.attr("x", function(d){ return xScale(parseYear(d.Year)) - (agtWidth/2); })
-			.attr("y",function(d,i){ return height - ((agtHeight + agtSpacing) * i) })
+			.classed("y" + i, true)
+			.attr("id", d => "rect" + d.AgtId)
+			.attr("x", function(d) {
+				return xScale(parseYear(d.Year)) - agtWidth / 2;
+			})
+			.attr("y", function(d, i) {
+				return height - (agtHeight + agtSpacing) * i;
+			})
 			.attr("width", agtWidth)
 			.attr("height", agtHeight)
-			.style('fill', '#000');
+			.style("fill", "#000");
 
-		rects.on("click", function(d) {
-			selectedAgt.clickOn(d)
-			event.stopPropagation();
-		})
-		.on("mouseover", onmouseover)
-		.on("mouseout", onmouseout);
-
+		rects
+			.on("click", function(d) {
+				selectedAgt.clickOn(d);
+				event.stopPropagation();
+			})
+			.on("mouseover", onmouseover)
+			.on("mouseout", onmouseout);
 	} // end for loop (years)
 
 	// add count at the top of each bar
-	var text = gBars.append('g')
+	var text = gBars
+		.append("g")
 		.selectAll("text .count")
-		.data(dataByYear)   //yrCounts format: [Year, yrCount]
+		.data(dataByYear) //yrCounts format: [Year, yrCount]
 		.enter()
 		.append("text")
-		.attr("class","count")
-		.attr("x", function(d){ 
-			return xScale(parseYear(d.key)) 
+		.attr("class", "count")
+		.attr("x", function(d) {
+			return xScale(parseYear(d.key));
 		})
-		.attr("y", function(d){ 
-			return height - (d.values.length * (agtHeight + agtSpacing)) 
+		.attr("y", function(d) {
+			return height - d.values.length * (agtHeight + agtSpacing);
 		})
-		.text(function(d){ return d.values.length; })
-
-
-
+		.text(function(d) {
+			return d.values.length;
+		});
 
 	// LINE CHART --------------------------------------------------------------
 
-	var tooltip = gLines.append("g")
-	    .attr("transform", "translate(-100,-100)")
-	    .attr("id", "tooltip")
+	var tooltip = gLines
+		.append("g")
+		.attr("transform", "translate(-100,-100)")
+		.attr("id", "tooltip")
 		.style("pointer-events", "none");
 
-	tooltip.append("circle")
+	tooltip
+		.append("circle")
 		.attr("class", "tooltipCircle")
-	    .attr("r", 3);
+		.attr("r", 3);
 
-	tooltip.append("text")
+	tooltip
+		.append("text")
 		.attr("class", "tooltipText")
-	    .attr("y", -12);
+		.attr("y", -12);
 
-	var dataLines = d3.nest()
-		.key(function(d) { return +d.Year; })
+	var dataLines = d3
+		.nest()
+		.key(function(d) {
+			return +d.Year;
+		})
 		.rollup(function(v) {
 			var arr = [];
 			codes.forEach(function(code) {
-				arr.push([code, d3.sum(v, function(d) { return d[code] > 2; })]); 
-			})
-			return arr
-			}) 
+				arr.push([
+					code,
+					d3.sum(v, function(d) {
+						return d[code] > 2;
+					})
+				]);
+			});
+			return arr;
+		})
 		.entries(data);
 
-	var flatDataLines = []
+	var flatDataLines = [];
 
 	for (var year = years[0]; year <= years[1]; year++) {
-		var index = dataLines.findIndex(function(d) {return d.key == year})
+		var index = dataLines.findIndex(function(d) {
+			return d.key == year;
+		});
 		if (index == -1) {
 			codes.forEach(function(code) {
 				flatDataLines.push({
 					year: year,
 					code: code,
 					count: 0
-				})
-			})
-		}
-		else {
+				});
+			});
+		} else {
 			dataLines[index].value.forEach(function(arr) {
 				flatDataLines.push({
 					year: year,
 					code: arr[0],
 					count: arr[1]
-				})
-			})
+				});
+			});
 		}
 	}
 
-	dataLines = d3.nest()
-		.key(function(d) {return d.code})
-		.entries(flatDataLines)
+	dataLines = d3
+		.nest()
+		.key(function(d) {
+			return d.code;
+		})
+		.entries(flatDataLines);
 
-	var maxCount = d3.max(flatDataLines, d => d.count)
+	var maxCount = d3.max(flatDataLines, d => d.count);
 
-	var yLines = d3.scaleLinear()
+	var yLines = d3
+		.scaleLinear()
 		.domain([0, maxCount])
-		.range([height, 0])
+		.range([height, 0]);
 
-	var yAxis = d3.axisLeft(yLines)
+	var yAxis = d3
+		.axisLeft(yLines)
 		.tickFormat(d3.format("d"))
 		.tickPadding([5]);
 
 	// draw axis into svg
-	gLines.append("g")
-		.attr("class","yaxis")
+	gLines
+		.append("g")
+		.attr("class", "yaxis")
 		.call(yAxis);
 
 	// define line generator
-	var line = d3.line()
+	var line = d3
+		.line()
 		.x(d => xScale(parseYear(d.year))) // set the x values for the line generator
-		.y(d => yLines(d.count)) // set the y values for the line generator 
+		.y(d => yLines(d.count)); // set the y values for the line generator
 
 	// draw lines
-	var lines1 = gLines.selectAll('.codeline')
+	var lines1 = gLines
+		.selectAll(".codeline")
 		.data(dataLines)
 		.enter()
 		.append("path")
-		.datum(d => d.values.sort(function(a,b) {return d3.ascending(a.year,b.year)}))
+		.datum(d =>
+			d.values.sort(function(a, b) {
+				return d3.ascending(a.year, b.year);
+			})
+		)
 		.attr("d", line)
-		.classed('codeline', true)
-		.attr('id', function(d) {return 'line' + d[0].code})
-		.style('stroke', d => codeColour(d[0].code))
-		.style('stroke-linejoin', "round")
-		.style('stroke-width', '2.5px')
-		.style('opacity', 0.6)
-		.style('fill', 'none')
-		.attr('pointer-events', 'none')
+		.classed("codeline", true)
+		.attr("id", function(d) {
+			return "line" + d[0].code;
+		})
+		.style("stroke", d => codeColour(d[0].code))
+		.style("stroke-linejoin", "round")
+		.style("stroke-width", "2.5px")
+		.style("opacity", 0.6)
+		.style("fill", "none")
+		.attr("pointer-events", "none");
 
 	// compute voronoi diagram for better mouseover interaction
-	const delaunay = d3.Delaunay.from(flatDataLines,
+	const delaunay = d3.Delaunay.from(
+		flatDataLines,
 		d => xScale(parseYear(d.year)), // x accessor
-		d => yLines(d.count)) 			// y accessor
+		d => yLines(d.count)
+	); // y accessor
 
 	const voronoi = delaunay.voronoi([0, 0, width, height]);
 
 	// draw (invisible) voronoi diagram
-	gLines.append('g')
-		.selectAll('path')
+	gLines
+		.append("g")
+		.selectAll("path")
 		.data(flatDataLines)
 		.enter()
-		.append('path')
-		.attr('d', function(d,i) { return voronoi.renderCell(i) })
-		.classed('voronoicell', true)
+		.append("path")
+		.attr("d", function(d, i) {
+			return voronoi.renderCell(i);
+		})
+		.classed("voronoicell", true)
 		// .style('stroke', 'black') // uncomment to see voronoi cells
-		.style('fill', 'transparent')
-		.on('mouseover', mouseover)
-		.on('mouseout', mouseout)
+		.style("fill", "transparent")
+		.on("mouseover", mouseover)
+		.on("mouseout", mouseout);
 
-	//Voronoi mouseover and mouseout functions	
+	//Voronoi mouseover and mouseout functions
 	function mouseover(d) {
-		lines1.style('opacity', 0.1)
-		d3.select('#line' + d.code).style('opacity', 0.6).moveToFront()
-		
+		lines1.style("opacity", 0.1);
+		d3.select("#line" + d.code)
+			.style("opacity", 0.6)
+			.moveToFront();
+
 		tooltip.moveToFront();
-	    tooltip.attr("transform", "translate(" + xScale(parseYear(d.year)) + "," + yLines(d.count) + ")");
-		tooltip.select(".tooltipCircle").style("fill", codeColour(d.code)).attr("r", 5);
-	    tooltip.select(".tooltipText").text(d.code + ': ' + d.count);
+		tooltip.attr(
+			"transform",
+			"translate(" + xScale(parseYear(d.year)) + "," + yLines(d.count) + ")"
+		);
+		tooltip
+			.select(".tooltipCircle")
+			.style("fill", codeColour(d.code))
+			.attr("r", 5);
+		tooltip.select(".tooltipText").text(d.code + ": " + d.count);
 	}
 
 	function mouseout(d) {
-		lines1.style('opacity', 0.6)	    
+		lines1.style("opacity", 0.6);
 		tooltip.attr("transform", "translate(-100,-100)");
 	}
-
 }
